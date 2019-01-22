100c7cd67557e924c3e0734823ab792d7ba38dab
Maciej Kołek <mkolek@getresponse.com>
2019-01-22 12:41:19 +0100

    add shop id

--- Classes/Etsy/Models/EtsyShop.swift
+++ Classes/Etsy/Models/EtsyShop.swift
@@ -12,16 +12,18 @@ open class EtsyShop: Decodable {
     public var name: String
     public var userId: Int
     public var iconUrl: String?
+    public var shopId: Int
     
     enum CodingKeys: String, CodingKey {
         case shopName = "shop_name"
         case userId = "user_id"
         case iconUrlFullxfull = "icon_url_fullxfull"
+        case shopId = "shop_id"
     }
     
     public required init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        
+        shopId = try values.decode(Int.self, forKey: .shopId)
         name = try values.decode(String.self, forKey: .shopName)
         userId = try values.decode(Int.self, forKey: .userId)
         iconUrl = try? values.decode(String.self, forKey: .iconUrlFullxfull)
